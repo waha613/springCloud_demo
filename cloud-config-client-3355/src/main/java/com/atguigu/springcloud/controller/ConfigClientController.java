ff68d432c0ad2ea386fd48d80bf48aa862f16281
liuzihao <251867818@qq.com>
2022-05-30 22:07:44 +0800

    f

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * @auther zzyy
+ * @create 2019-11-15 15:00
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
+
+
+
+
+
