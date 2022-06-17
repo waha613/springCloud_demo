95b66a96cae22e9b3ef0a607a4f58a691eb2fa43
liuzihao <251867818@qq.com>
2022-06-17 17:34:36 +0800

    f

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/MainApp8401.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author ll4859332@hotmail.com
+ * @version V1.0
+ * @title
+ * @description
+ * @date 2022-06-17 10:41
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401
+{
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ll4859332@hotmail.com
+ * @version V1.0
+ * @title
+ * @description
+ * @date 2022-06-17 10:57
+ */
+@RestController
+public class FlowLimitController
+{
+
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        return "------testB";
+    }
+}
